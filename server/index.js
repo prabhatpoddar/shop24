c0d115f923df1b515a1b510bb42dc2058100cdb6
prabhat poddar <pk041222@gmail.com>
2023-01-17 17:58:54 +0530

    bacic structure

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
-const express =require("express");
+const express = require("express");
 
-const app=express();
+const connect = require("./src/config/db");
 
+const app = express();
 
-
-app.listen(8080,()=>{
-    console.log("Server is running at port 8080")
-})
+app.listen(8080, () => {
+  try {
+    connect.then((res) => {
+      console.log("db is connected");
+    });
+    console.log("Server is running at port 8080");
+  } catch (error) {
+    console.log("error:", error);
+  }
+});
