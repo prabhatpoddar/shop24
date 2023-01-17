a9144a7b72936fd9d13559ba8e950262bd0d109b
prabhat poddar <pk041222@gmail.com>
2023-01-17 18:02:55 +0530

    bacic structure

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+require("dotenv").config()
+
 
 const connect = require("./src/config/db");
 
 const app = express();
 
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
   try {
     connect.then((res) => {
       console.log("db is connected");
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,6 +2,6 @@ const mongoose =require("mongoose");
 require("dotenv").config()
 mongoose.set('strictQuery', true);
 
-const connect=mongoose.connect(process.env.MONGO_URL)
+const connect=mongoose.connect(`${process.env.MONGO_URL}`)
 
 module.exports=connect
