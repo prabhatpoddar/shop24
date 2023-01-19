4f217fa982adebb021cdfcc80afc64b9393dd6a4
prabhat poddar <pk041222@gmail.com>
2023-01-19 18:37:14 +0530

    Admin panel

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -2,8 +2,18 @@ import MainRoute from "./Components/MainRouts"
 import Navbar from "./Components/Navbar/Navbar"
 import "./App.css"
 import Slider from "./Components/Slider/Slider"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getProductAdmin } from "./Redux/UserRedux/action"
 
 function App() {
+  const dispatch=useDispatch()
+  const data=useSelector(state=>state.userReducer.products)
+  console.log('data:', data)
+  useEffect(() => {
+    dispatch(getProductAdmin("user"))
+
+}, [dispatch])
 
   return (
     <div className="App">
@@ -11,8 +21,7 @@ function App() {
       <div id="container">
         <Slider/>
         <MainRoute/>
-      </div>
-   
+      </div> 
        
     </div>
   )
