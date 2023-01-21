4dfe4123c0fbf0b5df3b1ca47022851ded8d9134
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2023-01-21 09:01:59 +0530

    added

--- client/src/Components/MainRouts.jsx
+++ client/src/Components/MainRouts.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Beauty from "../Pages/Beauty/Beauty";
 import Login from "../Pages/Login/Login";
 import Signup from "../Pages/Login/Signup";
 
@@ -11,9 +10,9 @@ const MainRoute = () => {
       <Route path="/signup" element={<Signup/>}/>
 
 
-      <Route path="/personalcare" element={<Beauty/>}/>
+    
     </Routes>
   );
 };
-
+zz
 export default MainRoute;
