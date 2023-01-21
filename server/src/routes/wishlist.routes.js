472265cde775fae58cceebfe48743b1c0b2dadba
Sunilrath101 <sunilrath.ssr@gmail.comgit config --global user.email sunilrath.ssr@gmail.comgit config --global user.email sunilrath.ssr@gmail.com>
2023-01-21 10:51:35 +0530

    wishlist server side

---
+++ server/src/routes/wishlist.routes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const Wishlist = require("./../models/wishlist.model");
+const UserModel = require("../models/user.model");
+router.get("/", async (req, res) => {
+  try {
+    const items = await Wishlist.find();
+    res.send(items);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const payload = req.body;
+  const userId_Making_req = req.body.userID;
+  payload.userId = userId_Making_req;
+  try {
+    const user = await Wishlist.create(payload);
+    res.send(user);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await UserModel.findByIdAndDelete({ _id: id });
+    res.send("Note Deleted");
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+module.exports = router;
