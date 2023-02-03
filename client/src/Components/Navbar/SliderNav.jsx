92df6c65b4d4321bc5e42b4a170f01a164997ebf
prabhat poddar <pk041222@gmail.com>
2023-02-03 12:16:42 +0530

    new

--- client/src/Components/Navbar/SliderNav.jsx
+++ client/src/Components/Navbar/SliderNav.jsx
@@ -1,6 +1,9 @@
-import { Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, RadioGroup, useDisclosure } from '@chakra-ui/react'
+import { Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay,   Stack,  useDisclosure } from '@chakra-ui/react'
 import React from 'react'
 import { AiOutlineMenu } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import "./Slider.css"
+
 
 const SliderNav = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -13,11 +16,21 @@ const SliderNav = () => {
             <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
                 <DrawerOverlay />
                 <DrawerContent>
-                    <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+                    <DrawerHeader borderBottomWidth='1px'>
+        <Link to="/"><img src="https://user-images.githubusercontent.com/98205449/213233396-1caf5409-150c-4862-bb2b-03fbd8e3bbf5.jpg" alt="logo" className="logo" /></Link>
+                        
+                    </DrawerHeader>
                     <DrawerBody>
-                        <p>Some contents...</p>
-                        <p>Some contents...</p>
-                        <p>Some contents...</p>
+  <Stack spacing={2} direction='column'  >
+  <Link to="/mens" colorScheme="red"  borderColor="rgb(187, 186, 186)" color="black" >Mens</ Link>
+<Link to="/womens" colorScheme="red" borderColor="rgb(187, 186, 186)" color="black"> Womens</ Link>
+<Link to="/kids" colorScheme="red" borderColor="rgb(187, 186, 186)" color="black"> Kids</ Link>
+<Link to="/" colorScheme="red" borderColor="rgb(187, 186, 186)" color="black"> Home & Living</ Link>
+<Link to="/" colorScheme="red" borderColor="rgb(187, 186, 186)" color="black"> Beauty</ Link>
+<Link to="/" colorScheme="red" borderColor="rgb(187, 186, 186)" color="black"> Studio</ Link>
+
+
+  </Stack>
                     </DrawerBody>
                 </DrawerContent>
             </Drawer>
